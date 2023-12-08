29a449fb715787d7c0acc6666cc5a15a3704696f
jteng2127 <jteng2127@gmail.com>
2023-12-08 16:24:22 +0800

    feat: support multiple file pattern match

--- file_validator/cli.py
+++ file_validator/cli.py
@@ -20,7 +20,7 @@ def calculate(directory, output):
 @cli.command()
 @click.argument('sha1_json_path', type=click.Path(exists=True), nargs=2)
 @click.option('--output', '-o', default=f'{os.getcwd()}/validation_result.json', help='validation result json path')
-@click.option('--pattern', '-p', default='*', help='fnmatch pattern for file path')
+@click.option('--pattern', '-p', default=['*'], help='fnmatch pattern for file path', multiple=True)
 def validate(sha1_json_path, output, pattern):
     with open(sha1_json_path[0], 'r') as file:
         first_sha1_dict = json.load(file)
@@ -28,11 +28,11 @@ def validate(sha1_json_path, output, pattern):
     with open(sha1_json_path[1], 'r') as file:
         second_sha1_dict = json.load(file)
 
-    different_keys, first_extra_keys, second_extra_keys = compare_dicts(first_sha1_dict, second_sha1_dict, pattern)
+    different_keys, first_only_keys, second_only_keys = compare_dicts(first_sha1_dict, second_sha1_dict, pattern)
     json_dict = {
         'different': different_keys,
-        'extra_in_first': first_extra_keys,
-        'extra_in_second': second_extra_keys
+        'only_in_first': first_only_keys,
+        'only_in_second': second_only_keys
     }
 
     with open(output, 'w') as file:
